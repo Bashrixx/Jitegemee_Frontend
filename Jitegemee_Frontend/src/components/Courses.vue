e89f0715bb0e5c9e4a7804af3965f37c8860912f
Bashrixx <kaleliryan10081@gmail.com>
2025-06-24 12:32:10 +0300

    First Commit

---
+++ Jitegemee_Frontend/src/components/Courses.vue
@@ -0,0 +1,29 @@
+<script setup>
+    import {ref} from 'vue'
+    import {useCoursesStore} from '../stores/courses'
+    const coursesStore = useCoursesStore()
+    const courses = coursesStore.courses
+</script>
+
+<template>
+    <v-container>
+        <v-row>
+            <v-col md="4" v-for="course in courses">
+                <v-card color="indigo-lighten-2">
+                    <v-card-item>
+                        <v-card-title>{{ course.name }}</v-card-title>
+
+                        <v-card-subtitle>{{ course.school }}</v-card-subtitle>
+                    </v-card-item>
+
+                    <v-card-text>
+                        {{ course.description }}
+                    </v-card-text>
+                    <v-card-text>
+                        {{ course.intake }}
+                    </v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
---
+++ Jitegemee_Frontend/src/components/Events.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { ref } from 'vue'
+import { useEventsStore } from '../stores/events'
+const eventsStore = useEventsStore()
+const events = eventsStore.events
+</script>
+
+<template>
+    <v-container>
+        <v-row>
+            <v-col md="4" v-for="event in events">
+                <v-card color="indigo-lighten-2">
+                    <v-card-item>
+                        <v-card-title>{{ event.name }}</v-card-title>
+
+                        <v-card-subtitle>{{ event.image }}</v-card-subtitle>
+                    </v-card-item>
+                    <v-card-text>
+                        {{ event.date }}
+                    </v-card-text>
+                    <v-card-text>
+                        {{ event.location }}
+                    </v-card-text>
+                    <v-card-text>
+                        {{ event.category }}
+                    </v-card-text>
+
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
